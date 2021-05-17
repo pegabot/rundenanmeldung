aa30c1c9fb0b6ed327c7fd3cf8be0ca70d15fc3c
Gruselhaus <nico@gruselhaus.com>
2021-05-17 18:09:54 +0200

    Bootstrapping a form component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * Copyright (c) 2021 The Pegabot authors
+ * This code is licensed under GNU Affero General Public License v3.0
+ * (see https://github.com/pegabot/rundenanmeldung/blob/main/LICENSE for details)
+ */
+
+import { Form } from "@rjsf/bootstrap-4";
+import { JSONSchema7 } from "json-schema";
+import React from "react";
+import "./App.css";
+
+const schema: JSONSchema7 = {
+  title: "A registration form",
+  description: "A simple form example.",
+  type: "object",
+  required: ["firstName", "lastName"],
+  properties: {
+    firstName: {
+      type: "string",
+      title: "First name",
+      default: "Chuck",
+    },
+    lastName: {
+      type: "string",
+      title: "Last name",
+    },
+    telephone: {
+      type: "string",
+      title: "Telephone",
+      minLength: 10,
+    },
+  },
+};
+
+const log = (type: any) => console.log.bind(console, type);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form schema={schema} onChange={log("changed")} onSubmit={log("submitted")} onError={log("errors")} />
     </div>
   );
 }
